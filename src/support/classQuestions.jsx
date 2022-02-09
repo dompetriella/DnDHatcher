e42294752384d07cceb91c7182ca0f0d22d17d2c
Dominic Petriella <50121548+dompetriella@users.noreply.github.com>
2022-02-09 15:34:22 +0000

    class questions are done and fixed css overflow

--- src/support/classQuestions.jsx
+++ src/support/classQuestions.jsx
@@ -148,7 +148,7 @@ export const classQuestions = {
             a1: {
                 "text": "Yes",
                 "attribute" : "skill",
-                "value" : "Survival"
+                "value" : "survival"
             },
             a2: {
                 "text": "No",
@@ -176,98 +176,127 @@ export const classQuestions = {
         },
 
         {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
+            question: "What Olypmic sport do you win gold in?",
             a1: {
-                "text": "You quickly grab edge of the hole and leap out",
+                "text": "Gymnastics",
                 "attribute" : "skill",
-                "value" : "athletics"
+                "value" : "acrobatics"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "Archery",
                 "attribute" : "skill",
-                "value" : "nature"
+                "value" : "perception"
             },
-
             a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
+                "text": "Weightlifting",
                 "attribute" : "skill",
                 "value" : "survival"
             },
-            
-        }, 
+        },
+
+        {
+            question: "Your favorite subject in school was",
+            a1: {
+                "text": "History",
+                "attribute" : "skill",
+                "value" : "history"
+            },
+            a2: {
+                "text": "Science",
+                "attribute" : "skill",
+                "value" : "insight"
+            },
+            a3: {
+                "text": "Gym",
+                "attribute" : "skill",
+                "value" : "athletics"
+            },
+        },
     ],
 
     "monk": [
 
         {
-            question: "Are you good with animals?",
+            question: "If you were a superhero, you'd have the power of:",
             a1: {
-                "text": "Yes",
+                "text": "Invisibility",
                 "attribute" : "skill",
-                "value" : "animal handling"
+                "value" : "stealth"
             },
             a2: {
-                "text": "No",
+                "text": "Super Strength",
                 "attribute" : "skill",
-                "value" : "intimidation"
+                "value" : "athletics"
+            },
+
+            a3: {
+                "text": "Super Smarts",
+                "attribute" : "skill",
+                "value" : "insight"
+            },
+            
+            a4: {
+                "text": 'Talking to the "other side"',
+                "attribute" : "skill",
+                "value" : "religion"
             },
         },
 
         {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
+            question: "You can now time travel!  Are you going to the past or the future?",
             a1: {
-                "text": "You quickly grab edge of the hole and leap out",
+                "text": "Past",
                 "attribute" : "skill",
-                "value" : "athletics"
+                "value" : "history"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "Future",
                 "attribute" : "skill",
-                "value" : "nature"
+                "value" : "insight"
             },
-
-            a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
-                "attribute" : "skill",
-                "value" : "survival"
-            },
-            
-        }, 
+        },        
     ],
 
     "paladin": [
 
         {
-            question: "Are you good with animals?",
+            question: "Are you good at sports?  Be honest",
             a1: {
                 "text": "Yes",
-                "attribute" : "skill",
-                "value" : "animal handling"
-            },
-            a2: {
-                "text": "No",
-                "attribute" : "skill",
-                "value" : "intimidation"
-            },
-        },
-
-        {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
-            a1: {
-                "text": "You quickly grab edge of the hole and leap out",
                 "attribute" : "skill",
                 "value" : "athletics"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "No",
                 "attribute" : "skill",
-                "value" : "nature"
+                "value" : "insight"
+            },
+            
+            a3: {
+                "text": "Yes (but lying)",
+                "attribute" : "skill",
+                "value" : "persuasion"
+            },
+            
+        },
+
+        {
+            question: "Someone is bullying a friend of yours:",
+            a1: {
+                "text": "Bully them right back!  Don't talk to me or my friend ever again",
+                "attribute" : "skill",
+                "value" : "intimidation"
+            },
+            a2: {
+                "text": "Befriend the bully then poison his food",
+                "attribute" : "skill",
+                "value" : "medicine"
             },
 
             a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
+                "text": "Do nothing: karma will get him eventually",
                 "attribute" : "skill",
-                "value" : "survival"
+                "value" : "religion"
             },
             
         }, 
@@ -276,115 +305,184 @@ export const classQuestions = {
     "ranger": [
 
         {
-            question: "Are you good with animals?",
+            question: "Are you a good judge of character?",
             a1: {
                 "text": "Yes",
                 "attribute" : "skill",
-                "value" : "animal handling"
+                "value" : "perception"
             },
             a2: {
                 "text": "No",
                 "attribute" : "skill",
-                "value" : "intimidation"
+                "value" : "animal handling"
             },
         },
 
         {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
+            question: "You're hungry, but it's 3am:",
             a1: {
-                "text": "You quickly grab edge of the hole and leap out",
+                "text": "Sneak to the refridgerator in complete silence.  You are a ninja",
                 "attribute" : "skill",
-                "value" : "athletics"
+                "value" : "stealth"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "Snack from the herbs you have growing in your room",
                 "attribute" : "skill",
                 "value" : "nature"
             },
+        }, 
 
-            a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
+        {
+            question: "What is your best quality?",
+            a1: {
+                "text": "Your keen mind",
+                "attribute" : "skill",
+                "value" : "insight"
+            },
+            a2: {
+                "text": "Your ripped body",
                 "attribute" : "skill",
                 "value" : "survival"
             },
             
+            a3: {
+                "text": "Your iron will",
+                "attribute" : "skill",
+                "value" : "investigation"
+            },
         }, 
     ],
 
     "rogue": [
 
         {
-            question: "Are you good with animals?",
+            question: "How do you solve your problems?",
             a1: {
-                "text": "Yes",
+                "text": "Head-on",
                 "attribute" : "skill",
-                "value" : "animal handling"
+                "value" : "acrobatics"
             },
             a2: {
-                "text": "No",
+                "text": "A clever solution",
                 "attribute" : "skill",
-                "value" : "intimidation"
+                "value" : "sleight of hand"
+            },
+            a3: {
+                "text": "Cheat",
+                "attribute" : "skill",
+                "value" : "deception"
             },
         },
 
         {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
+            question: "Are you good at sports?  Be honest",
             a1: {
-                "text": "You quickly grab edge of the hole and leap out",
+                "text": "Yes",
                 "attribute" : "skill",
                 "value" : "athletics"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "No",
                 "attribute" : "skill",
-                "value" : "nature"
+                "value" : "insight"
+            },
+            
+            a3: {
+                "text": "Yes (but lying)",
+                "attribute" : "skill",
+                "value" : "persuasion"
+            },
+            
+        },
+
+        {
+            question: "What's your survival plan for the zombie apocalypse?",
+            a1: {
+                "text": "Move to the top of a lighthouse to watch it all unfold",
+                "attribute" : "skill",
+                "value" : "perception"
+            },
+            a2: {
+                "text": "Fight through the horde!",
+                "attribute" : "skill",
+                "value" : "athletics"
             },
 
             a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
+                "text": "Be scarier than the zombies, become their king",
                 "attribute" : "skill",
-                "value" : "survival"
+                "value" : "intimidation"
             },
             
         }, 
+
+        {
+            question: "Are you extroverted or introverted?",
+            a1: {
+                "text": "Extroverted",
+                "attribute" : "skill",
+                "value" : "performance"
+            },
+            a2: {
+                "text": "Introverted",
+                "attribute" : "skill",
+                "value" : "stealth"
+            },
+            
+        }, 
+        
     ],
 
     "sorcerer": [
 
         {
-            question: "Are you good with animals?",
+            question: "Do you like to read?",
             a1: {
-                "text": "Yes",
+                "text": "Reading is great!",
                 "attribute" : "skill",
-                "value" : "animal handling"
+                "value" : "insight"
             },
             a2: {
-                "text": "No",
+                "text": "Reading is for NERDS",
                 "attribute" : "skill",
                 "value" : "intimidation"
             },
         },
 
         {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
+            question: "Do you believe in love after life?",
             a1: {
-                "text": "You quickly grab edge of the hole and leap out",
+                "text": "Yes",
                 "attribute" : "skill",
-                "value" : "athletics"
+                "value" : "arcana"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "No",
                 "attribute" : "skill",
-                "value" : "nature"
+                "value" : "religion"
+            },
+        },
+
+        {
+            question: "Is lying neccessary sometimes?",
+            a1: {
+                "text": "Yes",
+                "attribute" : "skill",
+                "value" : "deception"
+            },
+            a2: {
+                "text": "No",
+                "attribute" : "skill",
+                "value" : "religion"
             },
 
             a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
+                "text": "It depends on the situation",
                 "attribute" : "skill",
-                "value" : "survival"
+                "value" : "persuasion"
             },
-            
-        }, 
+        },
+
     ],
 
     "warlock": [
@@ -404,63 +502,108 @@ export const classQuestions = {
         },
 
         {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
+            question: "Do you believe in love after life?",
             a1: {
-                "text": "You quickly grab edge of the hole and leap out",
+                "text": "Yes",
                 "attribute" : "skill",
-                "value" : "athletics"
+                "value" : "arcana"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "No",
+                "attribute" : "skill",
+                "value" : "religion"
+            },
+        },
+
+        {
+            question: "Someone stole your lunch from the refrigerator!",
+            a1: {
+                "text": "Investigate and find the culprit",
+                "attribute" : "skill",
+                "value" : "investigation"
+            },
+            a2: {
+                "text": "Make more food and lay in wait to catch the food thief",
+                "attribute" : "skill",
+                "value" : "deception"
+            },
+            a3: {
+                "text": "Jokes on you food stealer, it was all vegetables!",
                 "attribute" : "skill",
                 "value" : "nature"
             },
+        },        
 
-            a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
+        {
+            question: "Do you think about the past often?",
+            a1: {
+                "text": "Yes",
                 "attribute" : "skill",
-                "value" : "survival"
+                "value" : "history"
             },
-            
-        }, 
+            a2: {
+                "text": "No",
+                "attribute" : "skill",
+                "value" : "investigation"
+            },
+        },   
+
     ],
 
     "wizard": [
 
         {
-            question: "Are you good with animals?",
+            question: "Do you like magic tricks?",
             a1: {
-                "text": "Yes",
+                "text": "I love magic!",
                 "attribute" : "skill",
-                "value" : "animal handling"
+                "value" : "arcana"
             },
             a2: {
-                "text": "No",
+                "text": "Don't you dare pull a rabbit out of that hat",
                 "attribute" : "skill",
-                "value" : "intimidation"
+                "value" : "religion"
             },
         },
 
         {
-            question: "You're walking through the woods and the leaves below give way to a massive hole.. You're falling into a trap!",
+            question: "Your dream job as a little kid:",
             a1: {
-                "text": "You quickly grab edge of the hole and leap out",
+                "text": "Doctor",
                 "attribute" : "skill",
-                "value" : "athletics"
+                "value" : "medicine"
             },
             a2: {
-                "text": "You reach for a nearby vine and swing to safety",
+                "text": "President",
                 "attribute" : "skill",
-                "value" : "nature"
+                "value" : "history"
             },
-
             a3: {
-                "text": "You fall into the hole but you survive and climb out.  You're stronger than some measly trap!",
+                "text": "Detective",
                 "attribute" : "skill",
-                "value" : "survival"
+                "value" : "insight"
             },
-            
-        }, 
+        },  
+
+        {
+            question: "You've heard a rumor that you have a secret admirer",
+            a1: {
+                "text": "Ask a ouija board for answers",
+                "attribute" : "skill",
+                "value" : "arcana"
+            },
+            a2: {
+                "text": "Ask around and see if this person really exists",
+                "attribute" : "skill",
+                "value" : "investigation"
+            },
+            a3: {
+                "text": "Take an educated guess and confront them",
+                "attribute" : "skill",
+                "value" : "insight"
+            },
+        },  
+
     ]
     
 }
